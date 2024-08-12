101cbdd9e8d7049d37d888043a614063c5d68bb9
Emilio Lim <emilioarislim@gmail.com>
2024-08-12 10:51:37 -0700

    Finishing touches on matplotlib graphs for monthly and daily reports

--- main.py
+++ main.py
@@ -32,18 +32,13 @@ MONTHS = {
     'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6,
     'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12,
 }
-NUM_TO_MONTHS = {
-    1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun',
-    7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'
-}
-
 
 class DataNotFound(Exception):
     pass
 
 
 @dataclass
-class FileManager:
+class FlowerDataBase:
     filename: str = None
     file_import: pd.DataFrame = None
     connection: sql.Connection = sql.connect('flowershopdata.sqllite3')
@@ -108,35 +103,46 @@ class FileManager:
             rows = cur.fetchall()
             sales_over_time = {}
             for row in rows:
-                year = int(row[5][:4])
-                month = int(row[5][5:7])
-                day = int(row[5][8:])
-                date = dt.date(year, month, day)
-                if date not in sales_over_time:
-                    sales_over_time[date] = 1
+                sale_date = dt.datetime.strptime(row[5], '%Y-%m-%d').date()
+                month = sale_date.month
+                year = sale_date.year
+                day = sale_date.day
+                sale_date = dt.date(year, month, day)
+                if sale_date not in sales_over_time:
+                    sales_over_time[sale_date] = 1
                 else:
-                    sales_over_time[date] += 1
-            self._visualize_sales(sales_over_time, "Daily")
+                    sales_over_time[sale_date] += 1
+            if len(sales_over_time) > 0:
+                self._visualize_sales(sales_over_time, "Daily")
 
     def look_up_client(self):
         """Using user input, queries DB for a clients past buying history"""
         pass
 
-    def monthly_report(self):
-        """Uses visualize_sales() function to visualize monthly data of sales
-        Also gives information of client past buying history"""
+    def monthly_report(self, specify_year=2016):  # should be int
+        """
+        Uses visualize_sales() function to visualize monthly data of sales
+        Also gives information of client past buying history
+        """
         with self.connection as con:
             cur = con.cursor()
             cur.execute('SELECT * FROM flowershopdata')
             rows = cur.fetchall()
-            monthly_sales = {
-                "Jan": 0, "Feb": 0, "Mar": 0, "Apr": 0, "May": 0, "Jun": 0,
-                "Jul": 0, "Aug": 0, "Sep": 0, "Oct": 0, "Nov": 0, "Dec": 0,
-            }
+            monthly_sales = {}
+
+            # populate dictionary with date values
             for row in rows:
-                sale_month = dt.datetime.strptime(row[5], '%Y-%m-%d').date()
-                monthly_sales[NUM_TO_MONTHS[sale_month.month]] += 1
-            self._visualize_sales(monthly_sales, "Monthly")
+                sale_date = dt.datetime.strptime(row[5], '%Y-%m-%d').date()
+                month = sale_date.month
+                year = sale_date.year
+                if year == specify_year:
+                    first_of_month = dt.datetime(year, month, 1)
+                    if first_of_month not in monthly_sales:
+                        monthly_sales[first_of_month] = 1
+                    else:
+                        monthly_sales[first_of_month] += 1
+            if len(monthly_sales) > 0:
+                self._visualize_sales(monthly_sales, f"{specify_year} Monthly")
 
     def quarterly_report(self):
         """Uses visualize_sales() function to visualize quarterly
@@ -153,19 +159,33 @@ class FileManager:
         pass
 
     @staticmethod
-    def _visualize_sales(sales_over_time, specify_title="No specification"):
+    def _visualize_sales(sales_over_time,
+                         specify_title="No specification"):
         """Visualizes sales data using matplotlib"""
         # matplotlib
         fig, ax = plt.subplots()
         dates = sales_over_time.keys()
         sales = sales_over_time.values()
+
+        # defining width of bars in bar graph
+        total_dates = len(dates)
+        bar_width = 0.8
+        bar_edge = None
+        if total_dates < 12:
+            bar_width = 5
+        elif total_dates < 25:
+            bar_width = 3
+
         # set labels
         plt.xlabel("Time")
         plt.ylabel("Number of Customer Sales")
         plt.title(f"Customer Sales Over Time ({specify_title})")
+
         # create bar graph
-        ax.bar(dates, sales, color='g')
+        ax.bar(dates, sales, color='gold', edgecolor="blue", width=bar_width)
         ax.grid('on')
+        plt.xticks(rotation=20)
+        plt.subplots_adjust(bottom=0.15)
         ax.set_ylim(0, max(sales) + 1)
         plt.show()
 
@@ -187,12 +207,12 @@ class FileManager:
 
 
 def main():
-    file = FileManager('flowershopdata.csv')
+    # Test functions
+    file = FlowerDataBase('flowershopdata.csv')
     file.import_file()
     file.convert_file()
     file.daily_report()
-    #file.monthly_report()
-
+    #file.monthly_report(2016)
 
 
 if __name__ == '__main__':
